a42d31b63a2c8030a10340e06f92bd26934b5ed9
nourimanov <nourimanov@gmail.com>
2023-07-31 16:59:21 +0500

    success

--- limupa/blog/urls.py
+++ limupa/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from limupa.user.views import Index
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('blog/', Index.as_view(), name='index')
 ]
--- limupa/user/urls.py
+++ limupa/user/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from limupa.user.views import Index
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('user/', Index.as_view(), name='index')
 ]
