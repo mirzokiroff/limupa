ff367c7ed4f073cbd8a7e55f39ffd4ff5169fd5d
nourimanov <nourimanov@gmail.com>
2023-07-31 17:33:48 +0500

    success

--- limupa/shop/urls.py
+++ limupa/shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from limupa.shop.views import Index, Index2, Index3, Index4, WishList
+from limupa.shop.views import Index, Index2, Index3, Index4, WishList, ShoppingCart
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('shop/index2', Index2.as_view(), name='index2'),
-    path('shop/index3', Index3.as_view(), name='index3'),
-    path('shop/index4', Index4.as_view(), name='index4'),
-    path('shop/wishlist', WishList.as_view(), name='wishlist'),
+    path('shop/index2/', Index2.as_view(), name='index2'),
+    path('shop/index3/', Index3.as_view(), name='index3'),
+    path('shop/index4/', Index4.as_view(), name='index4'),
+    path('shop/wish-list/', WishList.as_view(), name='wishlist'),
+    path('shop/shopping-cart/', ShoppingCart.as_view(), name='shoppingcart'),
 ]
