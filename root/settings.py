3bafbc89e93d4cdbb1b5d764604f9e6ee7646108
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-07-31 17:26:49 +0500

    sys.path.append(os.path.join(BASE_DIR, 'apps')) sys.path.append(os.path.join(BASE_DIR, 'limupa')) ga uzgardi

--- root/settings.py
+++ root/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(os.path.join(BASE_DIR, 'apps'))
+sys.path.append(os.path.join(BASE_DIR, 'limupa'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
