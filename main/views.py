41a89731d4e6c57efc61685418c3694a43168a7a
nourimanov <nourimanov@gmail.com>
2023-07-31 15:55:29 +0500

    updated from me

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-# from main.models import author, about_me, my_tools
+from django.views.generic import TemplateView
 
 
-# Create your views here.
-def home(request):
-    return render(request, 'index.html')
+class Index(TemplateView):
+   template_name = 'index.html'
+
+
