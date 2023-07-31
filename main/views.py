7e6059819bcaeb24073adc28df99258658d60a6f
mirzokiroff <mirsidiq3421@gmail.com>
2023-07-31 15:45:22 +0500

    static lar

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-# from main.models import author, about_me, my_tools
+from django.views.generic import View, ListView, TemplateView
 
 
-# Create your views here.
-def home(request):
-    return render(request, 'index.html')
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+
+def NotfoundView(request):
+    return render(request, '404.html')
