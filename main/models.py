487687abf1aab861a22c9b790e83157c932f0f9d
Rozievich <blackcoder459@outlook.com>
2023-07-31 15:25:29 +0500

    Qayta yuklandi

--- main/models.py
+++ main/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Product(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
