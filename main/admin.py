bd044cbb43f23820be14dda502fe95d8595d2fa5
justlivefree <justlivefree2003@gmail.com>
2023-07-31 16:03:27 +0500

    main ochirdim

--- main/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-#hello
-# Register your models here.
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from main.views import Index
-# from main.views import HomeView
-
-urlpatterns = [
-    path('', Index.as_view, name='index'),
-    # path('', HomeView.as_view(), name='index'),
-    # path('404', notfound, name='404'),
-]
