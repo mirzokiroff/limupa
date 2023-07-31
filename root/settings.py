eb3992ab43cf81023f3e409ae7cce696b5fa9892
nourimanov <nourimanov@gmail.com>
2023-07-31 15:41:06 +0500

    settings updated

--- root/settings.py
+++ root/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sq9fyz532yrnupe=g@j4w3o*u68-@7(ylk@9hq8vk+^zceaewc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
