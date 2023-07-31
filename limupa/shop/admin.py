981da7a9dd8283dfa184f04f1697994de6dad1ac
justlivefree <justlivefree2003@gmail.com>
2023-07-31 18:50:56 +0500

    admin shop

--- limupa/shop/admin.py
+++ limupa/shop/admin.py
@@ -1,2 +1,4 @@
 from django.contrib import admin
-# Register your models here.
+from .models import GeneralProductCategory, ProductCategory
+
+admin.site.register((GeneralProductCategory, ProductCategory))
