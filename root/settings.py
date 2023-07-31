33a7936c6ef607011a21b9df07e0200ad420c6a5
Rozievich <blackcoder459@outlook.com>
2023-07-31 18:34:36 +0500

    New Update

--- root/settings.py
+++ root/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     # Installed apps
     'ckeditor',
     'ckeditor_uploader',
-
     # Custom apps
     'limupa.blog',
     'limupa.shop',
@@ -117,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
