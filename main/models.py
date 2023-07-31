330eb1b8b756f32548ee33914973bf4a5bb5dd57
Rozievich <blackcoder459@outlook.com>
2023-07-31 15:42:58 +0500

    Nimadir

--- main/models.py
+++ main/models.py
@@ -5,4 +5,8 @@ class Product(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('')
