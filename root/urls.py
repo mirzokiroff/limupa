708dddf2336612e9385877231a5a10f491981a44
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-07-31 18:39:40 +0500

    .

--- root/urls.py
+++ root/urls.py
@@ -6,5 +6,5 @@ from root import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('limupa.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
