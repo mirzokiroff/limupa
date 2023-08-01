0eab485057c7d68f71e7e3745896562712a7dfea
nourimanov <nourimanov@gmail.com>
2023-08-01 12:11:53 +0500

    vixuwe

--- limupa/blog/management/commands/add_blog.py
+++ limupa/blog/management/commands/add_blog.py
@@ -9,30 +9,3 @@ from faker import Faker
 class Command(BaseCommand):
     faker = Faker()
 
-    def handle(self, *args, **options):
-        post_all = Post.objects.all()
-
-        for _ in range(1000):
-            title = self.faker.sentence(6)
-            product = Post.objects.create(
-                title=title,
-                slug=slugify(title),
-                description=self.faker.text()
-            )
-            details = Detailed.objects.create(
-                price=random.randint(200, 10000),
-                quantity=random.randint(1, 1000),
-                detail=random.shuffle([15.3, 14.0, 12.0, 16.0]),
-                color=random.shuffle(Detailed.ProductColor.values),
-                size=random.shuffle(Detailed.SizeChoices.values)
-            )
-            product_image = ProductImage.objects.create(
-                is_general=True,
-                image='zbad5tfbbe5newoz03dw.jpeg'
-            )
-            for i in range(3):
-                product_image = ProductImage.objects.create(
-                    image='2-laptop-and-monitor.jpg'
-                )
-            product.categories.set([choice(product_all) for _ in range(3)])
-            product.company.set([choice(product_all) for _ in range(3)])
