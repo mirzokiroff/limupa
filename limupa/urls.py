5a674a580327600e703ff6116adbad64e1ff916a
nourimanov <nourimanov@gmail.com>
2023-08-01 11:28:19 +0500

    gg

--- limupa/urls.py
+++ limupa/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('post/', include('limupa.blog.urls')),
+    path('', include('limupa.blog.urls')),
     path('', include('limupa.shop.urls')),
-    path('users/', include('limupa.user.urls')),
+    path('', include('limupa.user.urls')),
 
 ]
