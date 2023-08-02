d4ab97b8feca5f1f97414878dbfb9b05fac970c4
mirzokiroff <mirsidiq3421@gmail.com>
2023-08-02 09:54:46 +0500

    1

--- limupa/urls.py
+++ limupa/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from shop.views import Index, W404
+from shop.views import Index, W404, AboutUs
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('users/', include('user.urls')),
     path('', Index.as_view(), name='index'),
     path('404/', W404.as_view(), name='404'),
+    path('aboutus/', AboutUs.as_view(), name='aboutus'),
 ]
